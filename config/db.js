b5ed24c86377c75097191a3b2e824a982da9b710
Aewajkhan <aewajkhan007@gmail.com>
2023-09-07 22:54:18 +0530

    NEW_ECOM

---
+++ config/db.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+
+// const connectDB=async()=>{
+//   try {
+//     console.log(`connect to port NO. ${process.env.PORT}`)
+//     return await mongoose.connect(process.env.MONGO_URL)
+//   } catch (error) {
+//     console.log("err",error)
+//   }
+// }
+export default connectDB;
